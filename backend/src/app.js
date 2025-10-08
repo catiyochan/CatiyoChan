c9a6941fc5619f5770cea42a1f2064142545b76d
Mohd Inzamam Ahmad <81466387+inzamam9494@users.noreply.github.com>
2025-10-08 13:56:31 +0530

    Add additional allowed origins for CORS configuration

--- backend/src/app.js
+++ backend/src/app.js
@@ -7,6 +7,8 @@ const app = express();
 const allowedOrigins = [
   "http://localhost:5173",
   "https://catiyo-chan.vercel.app",
+  "https://catiyochan.fun",
+  "https://www.catiyochan.fun"
 ]
 
 app.use(
